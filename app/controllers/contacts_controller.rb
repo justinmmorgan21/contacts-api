fb0201909c2c32bd2667e7e4f51f5b03f92af4c0
justinmmorgan21 <justinmorgan21@gmail.com>
2024-09-01 11:29:19 -0500

    add routes for first_contact and all_contacts

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,2 +1,21 @@
 class ContactsController < ApplicationController
+    def first_contact
+      contact = Contact.first
+      render json: { 
+        first_name: contact.first_name,
+        last_name: contact.last_name,
+        email: contact.email,
+        phone_number: contact.phone_number
+      }
+    end
+
+    def all_contacts
+      contacts = []
+      Contact.all.each { |contact|
+        contacts << contact
+      }
+      render json: {
+        contacts: contacts
+      }
+    end
 end
