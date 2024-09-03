5cf6cced01b5d5e7b9dd00d9a251701781345cd4
justinmmorgan21 <justinmorgan21@gmail.com>
2024-09-02 19:36:19 -0500

    update method syntax

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -2,33 +2,18 @@
 class ContactsController < ApplicationController
   
   def first_contact
-    contact = Contact.first
-    render json: { 
-      first_name: contact.first_name,
-      last_name: contact.last_name,
-      email: contact.email,
-      phone_number: contact.phone_number
-    }
+    @contact = Contact.first
+    render template: "contacts/show"
   end
 
   def all_contacts
-    contacts = []
-    Contact.all.each { |contact|
-      contacts << {
-        first_name: contact.first_name,
-        last_name: contact.last_name,
-        email: contact.email,
-        phone_number: contact.phone_number
-      }
-    }
-    render json: {
-      contacts: contacts
-    }
+    @contacts = Contact.all
+    render template: "contacts/index"
   end
 
   def to_terminal
     require 'net/http'
-    url = "http://localhost:3000/all_contacts"
+    url = "http://localhost:3000/all_contacts.json"
     response = HTTP.get(url)
     puts "Status Code: #{response.status}"
     puts "Response Body: #{response.to_s}"
